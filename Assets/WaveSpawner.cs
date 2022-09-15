b276cef86f565af4ff33ac91a991bb54d7080097
johnworker <78348941+johnworker@users.noreply.github.com>
2022-09-15 20:06:07 +0800

    添加遊戲控制腳本

---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace Leo
+{
+    public class WaveSpawner : MonoBehaviour
+    {
+        public Transform enemyPrefab;
+
+        public float timeBetweenWaves = 5f;
+        private float countdown = 2f;
+
+        void Update()
+        {
+            if(countdown <= 0f)
+            {
+                SpawnWave();
+                countdown = timeBetweenWaves;
+            }
+
+            countdown -= Time.deltaTime;
+        }
+
+        void SpawnWave()
+        {
+            Debug.Log("Wave Incomming");
+        }
+    }
+}
