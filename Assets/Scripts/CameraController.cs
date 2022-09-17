fdfa156ad28de59b0cd8833b210a3ca98db02209
johnworker <78348941+johnworker@users.noreply.github.com>
2022-09-17 11:22:42 +0800

    視野控制腳本添加

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace Leo
+{
+    public class CameraController : MonoBehaviour
+    {
+
+        public float panSpeed = 30f;
+        public float panBorderThickness = 10f;
+
+        void Update()
+        {
+            if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+            {
+                transform.Translate(Vector3.forward * panSpeed * Time.deltaTime,Space.World);
+            }
+
+            if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+            {
+                transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+            }
+
+            if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+            {
+                transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+            }
+
+            if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+            {
+                transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+            }
+
+        }
+    }
+}
