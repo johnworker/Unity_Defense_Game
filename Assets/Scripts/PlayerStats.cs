be23417708c4720458463368fcf10ba3b1369d82
johnworker <78348941+johnworker@users.noreply.github.com>
2022-09-18 18:18:39 +0800

    生命值文字設置

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -8,9 +8,13 @@ namespace Leo
         public static int Money;
         public int startMoney = 400;
 
+        public static int Lives;
+        public int startLives = 20;
+
         void Start()
         {
             Money = startMoney;
+            Lives = startLives;
         }
     }
 
